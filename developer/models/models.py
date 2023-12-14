ccc577a784eca1eef49ae1b4ec63725e973a7bc8
AlexSnowReDo <121876204+alexsnowredo@users.noreply.github.com>
2023-12-14 14:15:00 -0800

    [ADD] Primer Modulo

--- developer/models/models.py
+++ developer/models/models.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
 
-# class developer(models.Model):
-#     _name = 'developer.developer'
-#     _description = 'developer.developer'
+class developer(models.Model):
+    _name = 'developer.developer'
+    _description = 'developer.developer'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    Fecha_1 = fields.Date()
+    Fecha_nacimiento = fields.Date()
